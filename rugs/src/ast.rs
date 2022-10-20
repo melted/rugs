420abb0edb62efe15a69fba96f0c87d1909ad1d1
Niklas Larsson <niklas@mm.st>
2022-10-20 11:39:06 +0200

    Start on the AST data types

--- rugs/src/ast.rs
+++ rugs/src/ast.rs
@@ -1,7 +1,56 @@
 
+//! This module contains the AST data structures that are the output
+//! of the parsing state.
+
+use std::path::{ Path, PathBuf };
+use std::ops::Deref;
 
 pub struct Module {
+    pub name : String,
+    pub file : Option<PathBuf>,
+    pub pragmas : Located<Annotation>,
+    pub imports : Vec<Annotated<Import>>,
+    pub exports : Option<Vec<Annotated<String>>>
+}
 
+pub struct Import {
+    pub name : String,
+    pub qualified : bool,
+    pub alias : Option<String>,
+    pub specific : Option<Vec<String>>,
+    pub hidden : Vec<String>
+}
+
+pub struct Located<T> {
+    pub location : (usize, usize),
+    pub value : T
+}
+
+impl<T> Deref for Located<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.value
+    }
+}
+
+pub enum Annotation {
+    OtherPragma(String)
+}
+
+pub struct Annotated<T> {
+    pub annotations : Vec<Annotation>,
+    pub location : (usize, usize),
+    pub value : T
+}
+
+
+impl<T> Deref for Annotated<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.value
+    }
 }
 
 pub struct Expression {
--- rugs/src/parser.rs
+++ rugs/src/parser.rs
@@ -1,8 +1,32 @@
 mod tests;
+mod lexing;
 
 use crate::ast::Module;
 
-pub fn parse(code : &str) -> Result<Module, String> {
-    Err("bah!".to_string())
+pub (self) struct ParserState<'a> {
+
+}
+
+impl<'a> ParserState<'a> {
+    fn new() -> ParserState<'a> {
+        ParserState {  }
+    }
+}
+
+pub struct ParseError {
+    msg : String,
+    loc : (usize, usize)
+}
+
+impl ParseError {
+    pub fn new(what : &str, loc : (usize, usize)) -> ParseError {
+        ParseError { msg: what.to_string(), loc }
+    }
+}
+
+pub fn parse(code : &str) -> Result<Module, ParseError> {
+    let state = ParserState::new();
+
+    Err(ParseError::new("bah", (0,0)))
 }
 
